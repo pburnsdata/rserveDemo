d3e2e23639498c279e7c2de0b09847a05bdf1d7b
Preston Burns <Preston_Burns@rhoworld.com>
2019-02-01 11:22:10 -0500

    add clarifying comment in start_rserve.R

--- start_rserve.R
+++ start_rserve.R
@@ -9,4 +9,6 @@ packages <- c(
 
 lapply(packages, require, c = T)
 
+#run.RServe allows you to continue your current session. 
+#This prevents it from clearing the packages that were just loaded
 run.Rserve()
