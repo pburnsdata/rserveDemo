fbd43ceba386fc7f92ca47554747b1b06bac1f38
Preston Burns <Preston_Burns@rhoworld.com>
2019-02-01 11:20:04 -0500

    clean up R functions

--- lmer_outlier_command.R
+++ lmer_outlier_command.R
@@ -3,7 +3,7 @@
 # Uses csvs instead of Rserve
 #
 # On windows could run command like this for benchmarking:
-# Powershell -Command "Measure-Command {Rscript lmer_outlier_r.R [PATH]/data.csv 3}" 
+# Powershell -Command "Measure-Command {Rscript lmer_outlier_r.R [PATH]\data.csv 3}" 
 # (About 19x slower)
 
 library("lme4")
@@ -11,28 +11,25 @@ library("purrr")
 
 args = commandArgs(trailingOnly=TRUE)
 
-detect_outlier <- function(data_string, cutoff) {
+detect_outlier <- function(data, cutoff) {
 
-  
-  
-
-  #enforce types
+  # Enforce types
   data$site <-as.factor(data$site)
   data$subject <-as.factor(data$subject)
   data$time <-as.numeric(data$time)
   data$val <-as.numeric(data$val)
   
-  
-  
+  # Build mixed model 
   model <- lmer(val ~ site + (1|subject), data)
 
+  # Calculate Standardized Residuals
   res <- residuals(model)
   H <- hatvalues(model)
   sigma <- summary(model)$sigma
-  res <- map_dbl(1:length(res), ~ res[[.]]/(sigma*sqrt(1-H[[.]])))  #standardize
+  res <- map_dbl(1:length(res), ~ res[[.]]/(sigma*sqrt(1-H[[.]])))  
+  data$residuals <- abs(res) # being loose here but its a demo
   
-  data$residuals <- res # being loose here but its a demo
-  
+  # Label points as outliers if residuals are above cut off
   data$outlier <- ifelse(data$residuals >= cutoff, 1, 0)
 
   return(data)
@@ -41,6 +38,6 @@ detect_outlier <- function(data_string, cutoff) {
 
 data  <- read.csv(args[1])
 
-new_data <- detect_outlier(data, args[2])
+results <- detect_outlier(data, args[2])
 
-write.csv(new_data, "C:/Users/spburns/Desktop/longitudinal/fake_long_data_output.csv")
+write.csv(results, "C:/Users/spburns/Desktop/longitudinal/fake_long_data_output.csv")
--- src/main/resources/R/lmer_outlier.R
+++ src/main/resources/R/lmer_outlier.R
@@ -1,25 +1,27 @@
+# Longitudinal check
+
 detect_outlier <- function(data_string, cutoff) {
   
-  
+  # Read in String passed from Java client
   data  <- read.csv(text=data_string)
   
-  #enforce types
+  # Enforce types
   data$site <-as.factor(data$site)
   data$subject <-as.factor(data$subject)
   data$time <-as.numeric(data$time)
   data$val <-as.numeric(data$val)
   
-  
-  
+  # Build mixed model 
   model <- lmer(val ~ site + (1|subject), data)
   
+  # Calculate Standardized Residuals
   res <- residuals(model)
   H <- hatvalues(model)
   sigma <- summary(model)$sigma
-  res <- map_dbl(1:length(res), ~ res[[.]]/(sigma*sqrt(1-H[[.]])))  #standardize
+  res <- map_dbl(1:length(res), ~ res[[.]]/(sigma*sqrt(1-H[[.]])))  
+  data$residuals <- abs(res) # being loose here but its a demo
   
-  data$residuals <- res # being loose here but its a demo
-  
+  # Label points as outliers if residuals are above cut off
   data$outlier <- ifelse(data$residuals >= cutoff, 1, 0)
   
   return(data)
